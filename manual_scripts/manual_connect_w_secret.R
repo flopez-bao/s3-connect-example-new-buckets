557f838156bc22c6d3977452e4c6b581bb99ec5d
Fausto Lopez <flopez@baosystems.com>
2023-04-13 19:58:15 +0000

    - add documentation for reading, deleting files

--- manual_scripts/manual_connect_w_secret.R
+++ manual_scripts/manual_connect_w_secret.R
@@ -54,80 +54,65 @@ print(choices)
 # reset row names
 rownames(choices) <- NULL
 
-# read narratives data 
-  my_data <- "Narratives/Current_Frozen/xlsx/Narratives.xlsx"
-  data <- aws.s3::s3read_using(FUN = readxl::read_xlsx,
-                               trim_ws = TRUE, 
-                               bucket = my_bucket,
-                               object = my_data)
-  
-  head(data, 5)
-  
-
-  # read mer structured data
-  # read options
-  #read in data, fill in your bucket name and file name (object should hold the name of the file you want to read)
-  my_data <- "MER_Structured_Datasets/Current_Frozen/PSNU_Recent/txt/MER_Structured_Datasets_PSNU_IM_Recent_Ukraine.txt"
-  data <- aws.s3::s3read_using(FUN = readr::read_delim, "|", escape_double = FALSE,
+# read mer structured data
+# read options
+#read in data, fill in your bucket name and file name (object should hold the name of the file you want to read)
+my_data <- "MER_Structured_Datasets/Current_Frozen/PSNU_Recent/txt/MER_Structured_Datasets_PSNU_IM_Recent_Ukraine.txt"
+data <- aws.s3::s3read_using(FUN = readr::read_delim, "|", escape_double = FALSE,
                                trim_ws = TRUE, col_types = readr::cols(.default = readr::col_character()
                                ), 
                                bucket = my_bucket,
                                object = my_data)
   
-  head(data, 5)
+head(data, 5)
   
 
-# test writing --------------
+# test writing (EACH COMMUNITY APP IS GIVEN THEIR OWN PREFIX IN OUR S3 WORKSPACE) --------------
 
-# write to narratives workspace
+# an example would be system_narratives for the narratives application
+# confirm with out development team what your prefix is
+my_prefix = "my_prefix"
 
-  print("writing to narratives...")
-  s3write_using(mtcars, FUN = write.csv,
-                bucket = Sys.getenv("TEST_BUCKET_WRITE"),
-                object = "system_narratives/testing_fl.csv")
-  
-  # write to yoda workspace
-  
-  print("writing to yoda...")
-  s3write_using(mtcars, FUN = write.csv,
-               bucket = Sys.getenv("TEST_BUCKET_WRITE"),
-               object = "system_yoda/testing_fl.csv"
-  )
-  
-  # write to dreams workspace
-  
-  print("writing to system dreams...")
-  s3write_using(mtcars, FUN = write.csv,
-                bucket = Sys.getenv("TEST_BUCKET_WRITE"),
-                object = "system_dreams_saturation/testing_fl.csv"
-  )
-  
+# write to a workspace
+print("writing to narratives...")
+s3write_using(mtcars, FUN = write.csv,
+              bucket = Sys.getenv("TEST_BUCKET_WRITE"),
+              object = paste0(my_prefix, "/mycsv.csv")
+              )
 
 # test reading from write bucket --------
+s3read_using(FUN = read.csv,
+             bucket = Sys.getenv("TEST_BUCKET_WRITE"),
+             object = paste0(my_prefix, "/mycsv.csv")
+)
 
-
-  # write to narratives workspace
   
-  print("reading narratives data...")
-  s3read_using(FUN = read.csv,
-                bucket = Sys.getenv("TEST_BUCKET_WRITE"),
-                object = "system_narratives/testing_fl.csv"
+# list files for a prefix --------------
+  
+my_bucket <- Sys.getenv("TEST_BUCKET_WRITE")
+  
+# Lists all of bucket contents, fill in your bucket
+choices <- aws.s3::get_bucket(bucket = my_bucket, prefix = "system_dreams_saturation/")
+  
+# get just path names
+choices <- lapply(choices, "[[", 1)
+  
+# get just file names
+cleaned_choices <- lapply(choices, function(x) gsub(".*\\/", "", x))
+  
+# make dataframe of file names and path names
+choices <- do.call(rbind, Map(data.frame, file_names = cleaned_choices,
+                                path_names = choices, stringsAsFactors = FALSE))
+  
+# filter just files that end in txt or xlsx or csv
+choices <- choices[grepl("txt$|xlsx$|csv$|xlsx$", choices$file_names), ]
+print(choices)
+
+  
+  
+# delete an object ------
+aws.s3::delete_object(
+    object = paste0(my_prefix, "/testing.csv"),
+    bucket = Sys.getenv("TEST_BUCKET_WRITE")
   )
   
-  # write to yoda workspace
-  
-  print("reading yoda data...")
-  s3read_using(FUN = read.csv,
-                bucket = Sys.getenv("TEST_BUCKET_WRITE"),
-                object = "system_yoda/testing_fl.csv"
-  )
-  
-  # write to dreams workspace
-  
-  print("reading dreams data...")
-  s3read_using(FUN = read.csv,
-               bucket = Sys.getenv("TEST_BUCKET_WRITE"),
-               object = "system_dreams_saturation/testing_fl.csv"
-  )
-  
-
